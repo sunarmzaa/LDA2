eb8bb1acc7042bf817b2f091c2f637e72f09cb00
sunarmzaa <sunarmzaa@gmail.com>
2021-05-05 13:50:47 +0700

    7

--- templates/answers.py
+++ templates/answers.py
@@ -1,8 +1,8 @@
 import feedparser
 import csv
-news_rss_url = "https://answers.yahoo.com/rss/allq"
-info = feedparser.parse(news_rss_url)
+#news_rss_url = "https://answers.yahoo.com/rss/allq"
+#info = feedparser.parse(news_rss_url)
 with open('C:\\xampp\\htdocs\\prolda\\templates\\answers.txt','a') as fd:
     writer = csv.writer(fd, delimiter=',', lineterminator = '\n')
-    for entry in info.entries:
-        writer.writerow([entry.title])
+    #for entry in info.entries:
+        #writer.writerow([entry.title])
