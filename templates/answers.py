957883979789bbf1fb091042b97772439e318d5e
sunarmzaa <sunarmzaa@gmail.com>
2021-10-08 00:02:37 +0700

    lda

--- templates/answers.py
+++
@@ -1,8 +0,0 @@
-import feedparser
-import csv
-#news_rss_url = "https://answers.yahoo.com/rss/allq"
-#info = feedparser.parse(news_rss_url)
-with open('C:\\xampp\\htdocs\\prolda\\templates\\answers.txt','a') as fd:
-    writer = csv.writer(fd, delimiter=',', lineterminator = '\n')
-    #for entry in info.entries:
-        #writer.writerow([entry.title])
